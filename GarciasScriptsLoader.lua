0be6c94a82c37fcae7d6064f59a00c141fa3cad9
Kielsvu <jhoneragustin@gmail.com>
2026-02-03 20:31:10 +0800

    Add RebirthChampionUlti script URL to loader

--- GarciasScriptsLoader.lua
+++ GarciasScriptsLoader.lua
@@ -1,6 +1,7 @@
 local Scripts = {
     [136599248168660] = "https://raw.githubusercontent.com/KielKeyHandler/Util/refs/heads/main/SoloHunter.lua",
     [75992362647444] = "https://raw.githubusercontent.com/KielKeyHandler/Util/refs/heads/main/TapTapSimulator.lua",
+    [74260430392611] = "https://raw.githubusercontent.com/KielKeyHandler/Util/refs/heads/main/RebirthChampionUlti.lua",
 }
 
 local url = Scripts[game.GameId]
