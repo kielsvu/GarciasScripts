ad2e0f516ca66a3a452c2610a36011893938a8b6
Kielsvu <jhoneragustin@gmail.com>
2026-02-03 20:11:06 +0800

    Update GarciasScriptsLoader.lua

--- GarciasScriptsLoader.lua
+++ GarciasScriptsLoader.lua
@@ -1,6 +1,6 @@
 local Scripts = {
-    [136599248168660] = "https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/BloxFruit.lua",
-    [75992362647444] = "https://raw.githubusercontent.com/KielKeyHandler/Util/refs/heads/main/TapTapSimulator.Lua"
+    [136599248168660] = "https://raw.githubusercontent.com/KielKeyHandler/Util/refs/heads/main/SoloHunter.lua",
+    [75992362647444] = "https://raw.githubusercontent.com/KielKeyHandler/Util/refs/heads/main/TapTapSimulator.lua",
 }
 
 local url = Scripts[game.GameId]
