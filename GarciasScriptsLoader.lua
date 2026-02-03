d0b5939d56e39abff108ba99f7723242fb2f0d35
Kielsvu <jhoneragustin@gmail.com>
2026-02-03 20:05:47 +0800

    Update and rename GarciasScriptsLoader.Lua to GarciasScriptsLoader.lua

--- GarciasScriptsLoader.lua
+++ GarciasScriptsLoader.lua
@@ -1,5 +1,6 @@
 local Scripts = {
     [136599248168660] = "https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/BloxFruit.lua",
+    [75992362647444] = "https://raw.githubusercontent.com/KielKeyHandler/Util/refs/heads/main/TapTapSimulator.Lua"
 }
 
 local url = Scripts[game.GameId]
